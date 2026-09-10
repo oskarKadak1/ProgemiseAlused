5b8abce7543d67343246897372a7d5f92bc4196a
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-10 11:40:49 +0300

    IfAndElseNesting2

---
+++ IfAndElseNesting2/IfAndElseNesting2/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace IfAndElseNesting2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int y = 10;
+
+            if (y == 12)
+            {
+                Console.WriteLine("Y võrdub 12");
+            }
+            else if (y > 20)
+            {
+                if (y == 25)
+                {
+                    Console.WriteLine("Y võrdub 25");
+                }
+                else
+                {
+                    Console.WriteLine("Y on vahemikus 21 kuni 24 ja 26 kuni lõpmatus");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Mingid kahtlased numbrid jälle");
+            }
+
+
+        }
+    }
+}
