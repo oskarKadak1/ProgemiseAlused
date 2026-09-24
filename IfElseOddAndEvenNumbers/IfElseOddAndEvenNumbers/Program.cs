5295cec28a23d887d82b077e1b6c35fdd0b292cd
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-24 11:43:37 +0300

    IfElseOddAndEvenNumber2

--- IfElseOddAndEvenNumbers/IfElseOddAndEvenNumbers/Program.cs
+++ IfElseOddAndEvenNumbers/IfElseOddAndEvenNumbers/Program.cs
@@ -11,19 +11,34 @@
             //if ja eöse juures toimub kontroll, et
             //kas on paaris või paaritu nr
             string input = Console.ReadLine();
-            if (float.TryParse(input, out float number))
-            {
-                if (float.IsInteger(number / 2))
-                {
-                    Console.WriteLine("Number on Even");
-                }
-                else
-                {
-                    Console.WriteLine("Number on odd");
-                }
+            int number = int.Parse(input);
 
+            //% jagab muutujat number 2
+            //kui 2 asemel panna 3, siis jagab 3-ga
+            if (number %2 == 0)
+            {
+                Console.WriteLine("See on paarisarv " + input);
+                PaarisArv();
+                //siia tuleb välja kutsuda meetod,
+                //mis ütleb, et see on paarisarv
             }
-            
+            else
+            {
+                Console.WriteLine("See on paaritu number " + input);
+                PaarituArv();
+                //siia tuleb välja kutsuda meetod,
+                //mis ütleb, et see on paaritu arv
+            }
+
+        }
+
+        static void PaarituArv()
+        {
+            Console.WriteLine("Antud number on paaritu arv");
+        }
+        static void PaarisArv()
+        {
+            Console.WriteLine("Antud number on paarisarv");
         }
     }
 }
