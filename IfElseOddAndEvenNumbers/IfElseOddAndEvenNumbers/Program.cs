17a7c3df895fb0dbcc9c851627b7ccf133635208
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-24 11:11:51 +0300

    IfElseOddAndEvenNumbers

---
+++ IfElseOddAndEvenNumbers/IfElseOddAndEvenNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace IfElseOddAndEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta number");
+            //konsool küsib numbrit
+            //number tuleb ära parsida
+
+            //if ja eöse juures toimub kontroll, et
+            //kas on paaris või paaritu nr
+            string input = Console.ReadLine();
+            if (float.TryParse(input, out float number))
+            {
+                if (float.IsInteger(number / 2))
+                {
+                    Console.WriteLine("Number on Even");
+                }
+                else
+                {
+                    Console.WriteLine("Number on odd");
+                }
+
+            }
+            
+        }
+    }
+}
